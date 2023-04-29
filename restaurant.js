3ab95dd0b848e6f010368d35739c131402261db3
gargi <gaaaargi0809@gmail.com>
2023-04-29 13:42:28 +1000

    first commit

---
+++ restaurant.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app =express()
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile('./restaurant.html', { root: __dirname });
+});
+
+
+var server=app.listen(3020,()=>{
+
+    console.log("server is listening to port",server.address().port);
+})
+cdcd
